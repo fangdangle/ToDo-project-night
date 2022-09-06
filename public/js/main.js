ce75d5d40e1aad02acc2b2c3dbc9c48092104c1c
Ryan Hardin <79239181+Rhardin378@users.noreply.github.com>
2022-09-06 16:20:39 -0500

    Add files via upload
    couldn't get my pull request to work.

---
+++ public/js/main.js
@@ -0,0 +1,81 @@
+const deleteBtn = document.querySelectorAll('.del')
+const todoItem = document.querySelectorAll('span.not')
+const todoComplete = document.querySelectorAll('span.completed')
+
+Array.from(deleteBtn).forEach((el)=>{
+    el.addEventListener('click', deleteTodo)
+})
+
+Array.from(todoItem).forEach((el)=>{
+    el.addEventListener('click', markComplete)
+})
+
+Array.from(todoComplete).forEach((el)=>{
+    el.addEventListener('click', markIncomplete)
+})
+
+async function deleteTodo(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/deleteTodo', {
+            method: 'delete',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markComplete(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markComplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function markIncomplete(){
+    const todoId = this.parentNode.dataset.id
+    try{
+        const response = await fetch('todos/markIncomplete', {
+            method: 'put',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({
+                'todoIdFromJSFile': todoId
+            })
+        })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+    }catch(err){
+        console.log(err)
+    }
+}
+
+// Password visability toggle
+const password = document.querySelectorAll('.password')
+const togglePassword = document.querySelectorAll('.togglePassword').forEach(item =>{
+    item.addEventListener('click', function(){
+        password.forEach(item =>{
+            const type = item.getAttribute('type') === 'password' ? 'text' : 'password'
+            item.setAttribute('type', type)
+        })
+        item.classList.toggle('bi-eye')
+    })
+})
